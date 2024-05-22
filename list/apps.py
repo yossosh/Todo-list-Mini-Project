c37f83cc8b88a2d3f04667f023677308635c48e5
Sofiia <sonagm9@gmail.com>
2024-05-22 12:35:05 +0300

    used black for all files

--- list/apps.py
+++ list/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ListConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'list'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "list"
