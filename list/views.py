7ee50ae89a35a016c34e5f93a7e913342b9a8a45
Sofiia <sonagm9@gmail.com>
2024-05-22 12:42:10 +0300

    added home function in views

--- list/views.py
+++ list/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Tag, Task
+
+
+@login_required
+def home(request):
+    """View function for the home page of the site."""
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    tasks = Task.objects.all().order_by("is_done", "-created_at")
+    return render(
+        request,
+        "list/home.html",
+        {
+            "tasks": tasks,
+            "num_visits": num_visits,
+        },
+    )
