25b898bd40dad8368df0af3c17198c398731d8a4
Sofiia <sonagm9@gmail.com>
2024-05-22 13:31:06 +0300

    added home page and tag list functionality and templates

--- list/urls.py
+++ list/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from .views import (
     home,
+    TagListView,
 )
 
 app_name = "list"
 
 urlpatterns = [
     path("", home, name="home"),
+    path("tags/", TagListView.as_view(), name="tag_list"),
 ]
--- list/views.py
+++ list/views.py
@@ -1,21 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Tag, Task
 
-
 @login_required
 def home(request):
-    """View function for the home page of the site."""
-    num_visits = request.session.get("num_visits", 0)
-    request.session["num_visits"] = num_visits + 1
-
     tasks = Task.objects.all().order_by("is_done", "-created_at")
-    return render(
-        request,
-        "list/home.html",
-        {
-            "tasks": tasks,
-            "num_visits": num_visits,
-        },
-    )
+    return render(request, "list/home.html", {"tasks": tasks})
+
+
+class TagListView(LoginRequiredMixin, generic.ListView):
+    model = Tag
+    queryset = Tag.objects.order_by("id")
+    context_object_name = "tags"
