7f95ae46781b5434e85818230a519b3a0f83537b
Sofiia <sonagm9@gmail.com>
2024-05-22 14:38:43 +0300

    added forms, template for them and delete template

---
+++ list/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from .models import Task, Tag
+
+
+class TaskCreateForm(forms.ModelForm):
+    content = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": "Task Content"})
+    )
+    deadline = forms.DateTimeField(
+        widget=forms.DateTimeInput(attrs={"type": "datetime-local"}),
+        required=False
+    )
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple(),
+        required=False
+    )
+
+    class Meta:
+        model = Task
+        fields = ["content", "deadline", "tags"]
+
+
+class TaskUpdateForm(forms.ModelForm):
+    content = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": "Task Content"})
+    )
+    deadline = forms.DateTimeField(
+        widget=forms.DateTimeInput(attrs={"type": "datetime-local"}),
+        required=False
+    )
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple(),
+        required=False
+    )
+    is_done = forms.BooleanField(required=False)
+
+    class Meta:
+        model = Task
+        fields = ["content", "deadline", "tags", "is_done"]
